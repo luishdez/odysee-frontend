519b90ec0e062ae6528bd322955f180d15959a00
infinite-persistence <inf.persistence@gmail.com>
2022-01-12 11:28:33 +0800

    Use existing /$/signin but point to oauth instead.
    This should reduce changes, plus retains the link.

--- ui/component/sideNavigation/view.jsx
+++ ui/component/sideNavigation/view.jsx
@@ -81,7 +81,7 @@ const PREMIUM = {
 const UNAUTH_LINKS: Array<SideNavLink> = [
   {
     title: 'Log In',
-    link: `/$/${PAGES.OAUTH_LOGIN}`,
+    link: `/$/${PAGES.AUTH_SIGNIN}`,
     icon: ICONS.SIGN_IN,
   },
   {
