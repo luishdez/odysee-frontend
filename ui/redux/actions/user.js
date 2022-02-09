94bdcbd5f7e39f7aa1ceb4e9d889a8ce5cd48cd1
infinite-persistence <inf.persistence@gmail.com>
2022-02-09 10:57:47 +0800

    Revert "Populate has_verified_email for OIDC logins."
    This reverts commit 64e507ce9b07013d56c1a3b93c29bf545fa7cf8a.

--- ui/redux/actions/user.js
+++ ui/redux/actions/user.js
@@ -191,7 +191,7 @@ export function doAuthenticate(
       Lbryio.getTokens().then((tokens) => {
         dispatch({
           type: ACTIONS.AUTHENTICATION_SUCCESS,
-          data: { user, accessToken: tokens.access_token },
+          data: { user },
         });
 
         // if user is an Odysee member, get the membership details
