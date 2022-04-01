c2e6106851336b0672afae15fd1ff3340dea95f0
Anthony <contact@anthonymayfield.com>
2022-04-01 16:39:16 +0200

    adding tiers

--- ui/component/joinMembership/view.jsx
+++ ui/component/joinMembership/view.jsx
@@ -17,9 +17,11 @@ import WalletTipAmountSelector from 'component/walletTipAmountSelector';
 import { getStripeEnvironment } from 'util/stripe';
 const stripeEnvironment = getStripeEnvironment();
 
-const TAB_BOOST = 'TabBoost';
-const TAB_FIAT = 'TabFiat';
-const TAB_LBC = 'TabLBC';
+const TAB_TIER_1 = 'Tier1';
+const TAB_TIER_2 = 'Tier2';
+const TAB_TIER_3 = 'Tier3';
+const TAB_TIER_4 = 'Tier4';
+const TAB_TIER_5 = 'Tier5';
 
 type SupportParams = { amount: number, claim_id: string, channel_id?: string };
 type TipParams = { tipAmount: number, tipChannelName: string, channelClaimId: string };
@@ -60,8 +62,48 @@ type Props = {
   preferredCurrency: string,
 };
 
+let membershipTiers = [{
+  displayName: 'Helping Hand',
+  description: 'You\'re doing your part, thank you!',
+  monthlyContributionInUSD: 5,
+  perks: ['exclusiveAccess', 'badge'],
+}, {
+  displayName: 'Big-Time Supporter',
+  description: 'You are a true fan and are helping in a big way!',
+  monthlyContributionInUSD: 10,
+  perks: ['exclusiveAccess', 'earlyAccess', 'badge', 'emojis'],
+}, {
+  displayName: 'Community MVP',
+  description: 'Where would this creator be without you? You are a true legend!',
+  monthlyContributionInUSD: 20,
+  perks: ['exclusiveAccess', 'earlyAccess', 'badge', 'emojis', 'custom-badge'],
+}];
+
+const perkDescriptions = [{
+  perkName: 'exclusiveAccess',
+  perkDescription: 'You will exclusive access to members-only content',
+}, {
+  perkName: 'earlyAccess',
+  perkDescription: 'You will get early access to this creators content',
+}, {
+  perkName: 'badge',
+  perkDescription: 'You will get a generic badge showing you are a supporter of this creator',
+}, {
+  perkName: 'emojis',
+  perkDescription: 'You will get access to custom members-only emojis offered by the creator',
+}, {
+  perkName: 'custom-badge',
+  perkDescription: 'You can choose a custom badge showing you are an MVP supporter',
+}];
+
 export default function JoinMembership(props: Props) {
   const {} = props;
+
+  const [isOnConfirmationPage, setConfirmationPage] = React.useState(false);
+
+  const [activeTab, setActiveTab] = React.useState(TAB_TIER_1);
+
+  const tabButtonProps = { isOnConfirmationPage, activeTab, setActiveTab };
 
   return (
     <Form >
@@ -74,17 +116,17 @@ export default function JoinMembership(props: Props) {
           <>
             <div className="section">
               {/* tip fiat tab button */}
-              <TabSwitchButton label={__('Tier 1')} name={TAB_FIAT} />
+              <TabSwitchButton label={__('Tier 1')} name={TAB_TIER_1} {...tabButtonProps} />
 
               {/* tip LBC tab button */}
-              <TabSwitchButton label={__('Tier 2')} name={TAB_LBC} />
+              <TabSwitchButton label={__('Tier 2')} name={TAB_TIER_2} {...tabButtonProps} />
 
               {/* support LBC tab button */}
-              <TabSwitchButton label={__('Tier 3')} name={TAB_BOOST} />
+              <TabSwitchButton label={__('Tier 3')} name={TAB_TIER_3} {...tabButtonProps} />
 
-              <TabSwitchButton label={__('Tier 4')} name={TAB_BOOST} />
+              <TabSwitchButton label={__('Tier 4')} name={TAB_TIER_4} {...tabButtonProps} />
 
-              <TabSwitchButton label={__('Tier 5')} name={TAB_BOOST} />
+              <TabSwitchButton label={__('Tier 5')} name={TAB_TIER_5} {...tabButtonProps} />
             </div>
           </>
         }
